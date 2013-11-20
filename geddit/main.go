c5158f8c4b471c9ae154ec2e8b6f08175af5ce9f
Neil Middleton <neil@heroku.com>
2013-11-20 17:37:20 +0000

    Allow CLI to take subreddits as args

--- geddit/main.go
+++ geddit/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/neilmiddleton/reddit"
 	"log"
+	"os"
+	"strings"
 )
 
 func main() {
-	items, err := reddit.Get("golang")
 
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) < 2 {
+		log.Fatal(errors.New("Subreddit must be provided"))
 	}
 
-	for _, item := range items {
-		fmt.Println(item)
+	for _, subreddit := range os.Args[1:len(os.Args)] {
+		items, err := reddit.Get(subreddit)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("\n%s\n", strings.ToUpper(subreddit))
+		fmt.Println("=============")
+		for _, item := range items {
+			fmt.Println(item)
+		}
 	}
 }
